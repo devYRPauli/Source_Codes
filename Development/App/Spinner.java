36727fe5e8ce2cbe7850df7535fe97fb4cc0971b
Yash Raj Pandey <55940078+devYRPauli@users.noreply.github.com>
2020-01-28 20:10:59 +0530

    Create Spinner.java

---
+++ Development/App/Spinner.java
@@ -0,0 +1,22 @@
+package com.example.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void play(View view)
+    {
+        ImageView imageView=findViewById(R.id.imageView5);
+        imageView.animate().rotation(720);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
