ff12ea7414b53a6a3f35d6af64102f6c5cdf354b
Yash Raj Pandey <55940078+devYRPauli@users.noreply.github.com>
2020-01-29 10:45:43 +0530

    Update divisors.cpp

--- C++/divisors.cpp
+++ C++/divisors.cpp
@@ -5,82 +5,60 @@ using namespace std;
 set<loi> printDivisors(loi n)
 {
     set<loi> s;
-
 	for (loi i=1; i<=sqrt(n); i++)
 	{
 		if (n%i == 0)
 		{
 			if (n/i == i)
-				s.insert(i);
+			    s.insert(i);
 			else
-				s.insert(i), s.insert(n / i);
+			    s.insert(i), s.insert(n / i);
 		}
 	}
-
 	return s;
 }
 
 int main()
 {
 loi t;
-
 cin >> t;
-
 while(t--)
 {
     loi A, M;
-
     cin >> A >> M;
-
     set <loi> s = printDivisors(M);
-
     loi cnt = 0;
-
     set<loi> s1;
-
     set<loi> :: iterator it;
-
     for(it = s.begin(); it != s.end(); it++)
     {
         if((*it) == 1)
             continue;
-	    
-        else
-	{
+        else{
             if(((*it) - 1) % A == 0)
-	    {
+            {
                cnt++;
                loi y = *it;
-
-
                loi ans;
-
                if(y == M)
                {
-		       ans = (M - 1) / A;
+                   ans = (M - 1) / A;
                }
-
                else
-	       {
-		       ans = (y - 1) / A;
-		       ans *= (M / y);
-	       }
-
+               {
+                   ans = (y - 1) / A;
+                   ans *= (M / y);
+               }
                s1.insert(ans);
-	    }
+            }
         }
     }
-
     cout << cnt << "\n";
-
     for(it = s1.begin(); it != s1.end(); it++)
         cout << *it << " ";
-
     cout << "\n";
-
     s.clear();
     s1.clear();
-
 }
 	return 0;
 }
